08e4b8ea5d6578334e5cff9c7fa0599022f79a26
@abhishek.mohan <abhishek.mohan@surboard.se>
2023-09-22 01:12:09 +0530

    intial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  tagline = "An enthusiastic Computer Engineer who is committed to learning and self-development, so can consistently achieve better results. "
+}
