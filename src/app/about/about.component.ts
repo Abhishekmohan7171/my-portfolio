b9666e4af0c83a3f51898e1d24f784f37f6c56e2
@abhishek.mohan <abhishek.mohan@surboard.se>
2023-09-22 12:23:03 +0530

    skills added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  paraOne = "Hello there! 👋 I'm Abhishek Mohan , a computer engineering graduate from SRM University with a passion for continuous learning and a deep love for technology. My journey into the world of tech has been an exciting one, largely thanks to my involvement in various tech clubs and communities, where I've had the opportunity to explore and master a wide range of fascinating topics. One of my absolute favourite domains in the tech world is frontend development. The art of crafting beautiful web applications is something that truly ignites my creativity.";
+
+  paraTwo = "Currently, I'm proud to be a part of the talented team at Surfboard Payments, where I serve as a software engineer. In this role, I primarily work with Angular and Nest, harnessing these cutting-edge technologies to build robust and efficient solutions. My current focus is on a thrilling project - the development of a developer portal, where I get to channel my expertise and passion into creating a platform that empowers fellow developers. Outside of work, you can often find me diving deep into coding challenges, experimenting with new frameworks, or simply staying up-to-date with the latest trends in the tech world. I believe that the journey of learning never truly ends, and I'm always excited to take on new challenges and expand my skill set. I'm thrilled to be a part of the ever-evolving tech landscape and am constantly seeking new opportunities to contribute, learn, and grow.";
+
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -5,6 +5,45 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+export class SkillsComponent{
+
+  data = [
+    {
+      title:"Frontend Development",
+      content:"Proficient in Angular, React, HTML, CSS, and JavaScript, with a keen eye for crafting responsive and visually appealing user interfaces."
+    },
+    {
+      title:"CSS Frameworks",
+      content:"Experienced in working with Tailwind CSS and CSS, leveraging its utility-first approach to streamline and enhance frontend development."
+    },
+    {
+      title:"Backend Development",
+      content:"Skilled in server-side development using Nest.js, with a focus on building scalable and efficient APIs.Prototyping and Design: Proficient in using Figma to create wireframes, prototypes, and mockups, facilitating seamless collaboration between design and development teams."
+    },
+    {
+      title:"Version Control",
+      content:"Experienced in using Git and GitHub for version control, ensuring efficient collaboration and code management."
+    },
+    {
+      title:"Continuous Learning",
+      content:"Enthusiastic about staying updated with the latest trends and technologies in the ever-evolving field of web development."
+    },
+    {
+      title:"Team Collaboration",
+      content:"Skilled in working collaboratively within cross-functional teams, communicating effectively, and contributing to a positive work environment."
+    },
+    {
+      title:"Project Management",
+      content:"Capable of effectively managing tasks, timelines, and priorities to meet project goals and deliverables."
+    },
+    {
+      title:"User-Centric Design",
+      content:"Dedicated to creating user-friendly and accessible web applications that prioritize the end-user's experience."
+    },{
+
+      title:"Adaptability",
+      content:"Quick to adapt to new tools, technologies, and programming languages to meet the demands of diverse projects."
+    },
+  ]
 
 }
