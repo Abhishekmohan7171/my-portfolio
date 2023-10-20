a6384c2001f3942109f0922a9ad55f726f3a23c4
@abhishek.mohan <abhishek.mohan@surboard.se>
2023-10-20 15:27:00 +0530

    tablet view completed

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -47,24 +47,7 @@ export class SkillsComponent implements OnInit{
   ]
 
   ngOnInit(): void {
-    window.onscroll = (v) => {
-      console.log(v.target)
-const client = document.querySelector('.sec-2')?.getBoundingClientRect()
-console.log(client?.top, window.scrollY)
-
-      document.querySelectorAll('section').forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop;
-        let height = sec.offsetHeight;
-
-
-        if(top +400 >= offset){
-          sec.classList.add('show-animate')
-        }else{
-          sec.classList.remove('show-animate')
-        }
-      })
-    }
+    
   }
 
 }
